7b14586279e67426f9b93ca5f9c1c14fe350543f
ariapandu <ariapandu24@gmail.com>
2024-01-23 14:32:39 +0700

    index

--- Arsha/index.php
+++ Arsha/index.php
@@ -85,7 +85,7 @@ session_start();
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
           <h1>Mengenai Kemiskinan Di Indonesia</h1>
-          <h2>Indonesia Maju Bersama Prabowo</h2>
+          <h2>Maju Bersama PT Kebul Jaya</h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
             <a href="#about" class="btn-get-started scrollto">Get Started</a>
           </div>
